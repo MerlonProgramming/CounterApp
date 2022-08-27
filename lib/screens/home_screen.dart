138c714b7b6e55c8a34998fe3afb0079ebbfb655
Marlon <msdiazl@bancodeoccidente.com.co>
2022-08-26 20:12:10 -0500

    The function is extracted to separate the counter logic in which three functions are created and called in a StatelessWidget where you have the logic of the buttons

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -30,7 +30,7 @@ class HomeScreen extends StatelessWidget {
         ),
         //floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
         floatingActionButton: FloatingActionButton(
-            child: const Icon(Icons.add),
-            onPressed: (() => {counter++, print('Hola Mundo: $counter')})));
+            onPressed: (() => {counter++, print('Hola Mundo: $counter')}),
+            child: const Icon(Icons.add)));
   }
 }
